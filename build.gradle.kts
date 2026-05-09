cd181c9ab46f2044ca556d29e484d238e0d1767b
amasotti <toniomasotti@gmail.com>
2026-05-09 13:42:37 +0200

    feat: bootstrap project
    - gradle setup
    - compilation targets
    - properties & xml definition files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,63 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "2.3.20"
+    kotlin("plugin.serialization") version "2.3.20"
+    id("org.jetbrains.intellij.platform") version "2.6.0"
 }
 
 group = "com.tonihacks"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    intellijPlatform {
+        intellijIdeaCommunity("2024.2")
+        testFramework(TestFrameworkType.Platform)
+    }
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.mockk:mockk:1.13.13")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        id = "com.tonihacks.doppler-intellij"
+        name = "Doppler"
+        version = project.version.toString()
+        description = "Inject Doppler-managed secrets into JetBrains run configurations and browse/edit them from a tool window."
+        ideaVersion {
+            sinceBuild = "242"
+        }
+    }
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+    }
 }
 
 kotlin {
     jvmToolchain(25)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_21)
 }
 
 tasks.test {
